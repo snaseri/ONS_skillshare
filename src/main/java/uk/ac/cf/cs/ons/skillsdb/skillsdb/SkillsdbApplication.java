e34bcc87489669624634e1bc0c897d9e42b5e27a
Kyle Morris <MorrisK10@Cardiff.ac.uk>
2019-11-15 00:09:42 +0000

    Added Spring Boot.

---
+++ src/main/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/SkillsdbApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.cs.ons.skillsdb.skillsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/SkillsdbApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.cs.ons.skillsdb.skillsdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
