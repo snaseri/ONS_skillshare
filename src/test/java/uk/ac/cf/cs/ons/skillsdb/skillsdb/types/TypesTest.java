5a9f0ab16349a98b9ffcb1de7594ccbf7c11d294
Kyle Morris <MorrisK10@Cardiff.ac.uk>
2019-11-19 23:08:32 +0000

    Updated TypeTest not to use jUnit API.

--- src/test/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/types/TypesTest.java
+++ src/test/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/types/TypesTest.java
@@ -1,7 +1,7 @@
 package uk.ac.cf.cs.ons.skillsdb.skillsdb.types;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,14 +12,14 @@ public class TypesTest {
     @Before
     public void setup() {
       type = new Type()
-              .id(1)
+              .id(1L)
               .name(AdvertType.ADVICE);
     }
 
     @Test
     public void typeIdCanChange() {
-      type.id(2);
-      assertEquals( 2, type.id() );
+      type.id(2L);
+      assertEquals( 2L, type.id() );
     }
 
     @Test
