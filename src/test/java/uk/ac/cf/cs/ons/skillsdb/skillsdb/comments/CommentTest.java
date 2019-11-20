7668e1b9127c36d27152dea8a6c55c620335895c
Sepehr Naseri <NaseriS@cardiff.ac.uk>
2019-11-20 03:21:05 +0000

    Add comment repository and interface for database interaction

---
+++ src/test/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/comments/CommentTest.java
@@ -0,0 +1,28 @@
+package uk.ac.cf.cs.ons.skillsdb.skillsdb.comments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentTest {
+
+    @Test
+    void id() {
+    }
+
+    @Test
+    void title() {
+    }
+
+    @Test
+    void description() {
+    }
+
+    @Test
+    void advert() {
+    }
+
+    @Test
+    void user() {
+    }
+}
