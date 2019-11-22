1acf7dede51f63002dc1cce6ab3cc0fb59f7da0b
Kyle Morris <MorrisK10@Cardiff.ac.uk>
2019-11-22 03:26:28 +0000

    Added a default user to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,5 @@ INSERT INTO Types(id, name) VALUES (3, 'ADVICE');
 INSERT INTO Skills(id, name, description) VALUES (1, 'Python', 'Programming with Python.');
 INSERT INTO Skills(id, name, description) VALUES (2, 'Java', 'Programming with Java.');
 INSERT INTO Skills(id, name, description) VALUES (3, 'SQL', 'Programming with SQL.' );
+
+INSERT INTO Users(id, username, password) VALUES (1, 'Administrator', 'Password');
