65b77c6972f12c552d7941956cded5d0b0542e3d
c1645271 <HanifH@cardiff.ac.uk>
2019-11-25 14:37:09 +0000

    Lombok config changed

--- src/main/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/users/UserRepoJPA.java
+++ src/main/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/users/UserRepoJPA.java
@@ -5,9 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface UserRepoJPA extends UserRepository, CrudRepository<uk.ac.cf.cs.ons.skillsdb.skillsdb.users.User, Long> {
+public interface UserRepoJPA extends UserRepository, CrudRepository<User, Long> {
 
     @Query(value = "select * from Users where upper(concat(username)) like concat('%', upper(:paramSearch), '%')", nativeQuery = true)
     List<User> findByUsername(@Param("paramSearch")String searchTerm);
