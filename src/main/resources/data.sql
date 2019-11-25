4b5c069b9534829b6d1461cd357f9d752c685ac9
Kyle Morris <MorrisK10@Cardiff.ac.uk>
2019-11-25 16:11:44 +0000

    Added Data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,9 @@ INSERT INTO Types(id, name) VALUES (1, 'ACCELERATION');
 INSERT INTO Types(id, name) VALUES (4, 'ASSISTANCE');
 INSERT INTO Types(id, name) VALUES (2, 'TUTORING');
 INSERT INTO Types(id, name) VALUES (3, 'ADVICE');
+
+INSERT INTO Skills(id, name, description) VALUES (1, 'Python', 'Programming with Python.');
+INSERT INTO Skills(id, name, description) VALUES (2, 'Java', 'Programming with Java.');
+INSERT INTO Skills(id, name, description) VALUES (3, 'SQL', 'Programming with SQL.' );
+
+INSERT INTO Users(id, username, password) VALUES (1, 'Administrator', 'Password');
