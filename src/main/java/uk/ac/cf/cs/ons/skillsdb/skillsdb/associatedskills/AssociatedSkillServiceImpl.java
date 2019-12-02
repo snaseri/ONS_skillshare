c6def083571ee406c01d519812543ef27d9b9ab7
c1645271 <HanifH@cardiff.ac.uk>
2019-12-02 16:21:33 +0000

    Added controller and implemented service.Template made nothing inside of it.

--- src/main/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/associatedskills/AssociatedSkillServiceImpl.java
+++ src/main/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/associatedskills/AssociatedSkillServiceImpl.java
@@ -1,4 +1,23 @@
 package uk.ac.cf.cs.ons.skillsdb.skillsdb.associatedskills;
 
-public class AssociatedSkillServiceImpl {
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AssociatedSkillServiceImpl implements AssociatedSkillService {
+
+    private AssociatedSkillRepository associatedSkillRepository;
+
+    public AssociatedSkillServiceImpl(AssociatedSkillRepository repository){associatedSkillRepository = repository;}
+
+    @Override
+    public Optional<AssociatedSkill> findByASIndex(long id){return associatedSkillRepository.findById(id);}
+
+    @Override
+    public List<AssociatedSkill> findBySkillName(String searchTerm) {
+        return associatedSkillRepository.findABySkillName(searchTerm);
+    }
 }
