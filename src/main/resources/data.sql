de4393f83dcd0358f32e923fd1bfa9197fe16cb0
Sepehr Naseri <NaseriS@cardiff.ac.uk>
2019-12-02 13:39:31 +0000

    added fake data for course

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -50,3 +50,10 @@ insert into Users (id, username, password) values (26, 'gkeatp', 'PWEUYqAJF');
 insert into Users (id, username, password) values (25, 'cnazairo', '9B4bUm');
 insert into Users (id, username, password) values (39, 'Robert', 'password');
 insert into Users (id, username, password) values (40, 'Rob', 'password');
+
+insert into Courses (id, name, description, is_free, price, posted, user_creator, skill_id) values (1, 'Java', 'Mauvvvvvvvvvvvvv', false, 2.89, '2008-11-11 13:23:44', 1, 2);
+insert into Courses (id, name, description, is_free, price, posted, user_creator, skill_id) values (2, 'Python', 'a very coooooool course', true, 0, '2010-11-11 13:23:44', 1, 3);
+insert into Courses (id, name, description, is_free, price, posted, user_creator, skill_id) values (3, 'The Course', 'for fun and a good time', true, 0, '2019-11-11 13:23:44', 1, 1);
+insert into Courses (id, name, description, is_free, price, posted, user_creator, skill_id) values (4, 'Free flask', 'a very practical course', true, 0, '2019-11-11 13:23:44', 1, 1);
+insert into Courses (id, name, description, is_free, price, posted, user_creator, skill_id) values (5, 'Skill share', 'practical and lots of fun', false, 5, '2020-11-11 13:23:44', 1, 2);
+
