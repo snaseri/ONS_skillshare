19f845f9214de20629cf850d7c96c4ea9d19453b
Sepehr Naseri <NaseriS@cardiff.ac.uk>
2019-12-07 16:28:58 +0000

    Added a service,jparepo and a domain for skill hirerachy

---
+++ src/main/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/skilltaxonomy/TaxonomyController.java
@@ -0,0 +1,4 @@
+package uk.ac.cf.cs.ons.skillsdb.skillsdb.skilltaxonomy;
+
+public class TaxonomyController {
+}
---
+++ src/main/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/skilltaxonomy/TaxonomyRepository.java
@@ -0,0 +1,51 @@
+package uk.ac.cf.cs.ons.skillsdb.skillsdb.courses;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * CourseRepository allows the caller to access to the courses data source.
+ *
+ * @author  Sepehr Naseri
+ * @version 1.0
+ * @since   2019-11-20
+ * @see     Course
+ * @url     https://spring.io/guides/gs/accessing-data-jpa/
+ */
+
+
+public interface CourseRepository {
+
+    /**
+     * Find a Comment by id.
+     *
+     * @param id Of the course you want to find.
+     * @return Optional Of the selected course.
+     */
+    Optional<Course> findById(Long id);
+
+
+    /**
+     * Find a Comment by name.
+     *
+     * @param name Of the course you want to find.
+     * @return List Of the selected courses.
+     */
+    List<Course> findByName(String name);
+
+    /**
+     * Find a Comment by price.
+     *
+     * @param price Of the course you want to find.
+     * @return List Of the courses with selected price.
+     */
+    List<Course> findByPrice(Double price);
+
+
+
+    public Course save(Course aCourse);
+
+
+}
+
+
