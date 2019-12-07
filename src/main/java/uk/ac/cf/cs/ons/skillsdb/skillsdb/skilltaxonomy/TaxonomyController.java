a765c2c3bcb9552ec28e7fd7221c729ca115f99d
Sepehr Naseri <NaseriS@cardiff.ac.uk>
2019-12-07 17:07:33 +0000

    filtred search for skills

--- src/main/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/skilltaxonomy/TaxonomyController.java
+++ src/main/java/uk/ac/cf/cs/ons/skillsdb/skillsdb/skilltaxonomy/TaxonomyController.java
@@ -7,24 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import uk.ac.cf.cs.ons.skillsdb.skillsdb.courses.Course;
+import uk.ac.cf.cs.ons.skillsdb.skillsdb.skills.SkillRepository;
 
 import javax.validation.Valid;
 
 @Controller
 public class TaxonomyController {
 
-    public TaxonomyController() {
+    private SkillRepository skillRepo;
+
+    public TaxonomyController(SkillRepository sRepo) {
+        skillRepo = sRepo;
     }
 
 
     @GetMapping(path = "/discover")
     public String createCourse(Model model) {
-
-        return "discover";
+        model.addAttribute("skills", skillRepo.findAll());
+        return "discover/skilldiscover";
     }
 
     @PostMapping("/discover")
     public String submitCourse(@ModelAttribute("course") @Valid Course course, BindingResult result, Model model) {
-        return "discover";
+        return "discover/skilldiscover";
     }
 }
